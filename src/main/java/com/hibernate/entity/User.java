c4476647bce8ebb3c88f3997395999d54b7c04e3
Sergei-JD <sergshlyazhko@gmail.com>
2022-02-11 11:10:21 +0300

    added MappedSuperclass

--- src/main/java/com/hibernate/entity/User.java
+++ src/main/java/com/hibernate/entity/User.java
@@ -18,7 +18,7 @@ import java.util.List;
 @EqualsAndHashCode(of = "username")
 @Table(name = "users", schema = "public")
 @TypeDef(name = "jsonbName", typeClass = JsonBinaryType.class)
-public class User implements Comparable<User> {
+public class User implements Comparable<User>, BaseEntity<Long> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
