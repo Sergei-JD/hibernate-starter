8098be4c42aa46a7640eebc3232cde157721bc18
Sergei-JD <sergshlyazhko@gmail.com>
2022-02-13 09:43:56 +0300

    implemented Transactions: Locks. Pessimistic

--- src/main/java/com/hibernate/entity/Payment.java
+++ src/main/java/com/hibernate/entity/Payment.java
@@ -1,37 +1,35 @@
 package com.hibernate.entity;
 
-import lombok.Data;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
-import org.hibernate.annotations.DynamicUpdate;
-import org.hibernate.annotations.OptimisticLocking;
-import org.hibernate.annotations.OptimisticLockType;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Id;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Version;
 
 @Data
 @Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@OptimisticLocking(type = OptimisticLockType.ALL)
-@DynamicUpdate
+//@OptimisticLocking(type = OptimisticLockType.ALL)
+//@DynamicUpdate
 public class Payment implements BaseEntity<Long> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-//    @Version
-//    private Long version;
+    @Version
+    private Long version;
 
     @Column(nullable = false)
     private Integer amount;
